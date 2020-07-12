974cfb024005cc0186dbd47674a8985ba3429fb5
Jamie Cansdale <jcansdale@gmail.com>
2020-07-12 23:50:59 +0100

    Add index.js entry point

---
+++ index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const path = require('path');
+const { spawn } = require('child_process');
+
+let file = path.join(__dirname, "dotnet");
+const child = spawn(file, process.argv.slice(2));
+
+child.stdout.on('data', (data) => {
+  console.log(`${data}`);
+});
+
+child.stderr.on('data', (data) => {
+  console.error(`${data}`);
+});
+
+child.on('close', (code) => {
+  process.exit(code)
+});
