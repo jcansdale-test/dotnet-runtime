37225b278c005c882846e5d4dc7b4cb4d261712d
Jamie Cansdale <jcansdale@gmail.com>
2020-07-16 00:38:33 +0100

    Simplify

--- dotnet.js
+++ dotnet.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
+
 const command = require('./index')
+
 if (!command) {
-    let platform = process.platform
-    console.error(`The ${platform} platform isn't currently supported.`)
-    process.exit(1)
+  console.error(`The ${process.platform} platform isn't currently supported.`)
+  process.exit(1)
 }
 
 const { spawn } = require('child_process');
 
-const child = spawn(command, process.argv.slice(2), { stdio: 'inherit' });
+const args = process.argv.slice(2)
+const child = spawn(command, args, { stdio: 'inherit' })
 
 child.on('close', (code) => {
   process.exit(code)
-});
+})
